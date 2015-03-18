3ddacdf43a55077587fe0237fcf0cbae602e26c5
Sandeep Prakash <123sandy@gmail.com>
2015-03-17 21:43:42 -0700

    NFC Code from third party.
    NFC Code from third party: https://github.com/nadam/nfc-reader
    
    Gradle build failing.

---
+++ android-application/LightControlNFC/app/src/main/java/com/bangaloretalkies/lightcontrolnfc/NdefMessageParser.java
@@ -0,0 +1,75 @@
+package com.bangaloretalkies.lightcontrolnfc;
+
+/**
+ * Created by Sandeep on 3/17/2015.
+ */
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bangaloretalkies.lightcontrolnfc.record.ParsedNdefRecord;
+import com.bangaloretalkies.lightcontrolnfc.record.SmartPoster;
+import com.bangaloretalkies.lightcontrolnfc.record.TextRecord;
+import com.bangaloretalkies.lightcontrolnfc.record.UriRecord;
+import android.app.Activity;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Utility class for creating {@link ParsedNdefRecord}s.
+ */
+public class NdefMessageParser {
+
+    // Utility class
+    private NdefMessageParser() {
+
+    }
+
+    /** Parse an NdefMessage */
+    public static List<ParsedNdefRecord> parse(NdefMessage message) {
+        return getRecords(message.getRecords());
+    }
+
+    public static List<ParsedNdefRecord> getRecords(NdefRecord[] records) {
+        List<ParsedNdefRecord> elements = new ArrayList<ParsedNdefRecord>();
+        for (final NdefRecord record : records) {
+            if (UriRecord.isUri(record)) {
+                elements.add(UriRecord.parse(record));
+            } else if (TextRecord.isText(record)) {
+                elements.add(TextRecord.parse(record));
+            } else if (SmartPoster.isPoster(record)) {
+                elements.add(SmartPoster.parse(record));
+            } else {
+                elements.add(new ParsedNdefRecord() {
+                    @Override
+                    public View getView(Activity activity, LayoutInflater inflater, ViewGroup parent, int offset) {
+                        TextView text = (TextView) inflater.inflate(R.layout.tag_text, parent, false);
+                        text.setText(new String(record.getPayload()));
+                        return text;
+                    }
+
+                });
+            }
+        }
+        return elements;
+    }
+}
